7aaa5569cf066411a44587796977480f25d228c5
wkw <rced87@aliyun.com>
2020-05-19 20:53:06 +0800

    删除这几个实体类

--- webserve/src/test/java/com/vikey/webserve/WebserveApplicationTests.java
+++ webserve/src/test/java/com/vikey/webserve/WebserveApplicationTests.java
@@ -1,13 +1,19 @@
 package com.vikey.webserve;
 
+import com.vikey.webserve.service.IAnnexe_taskService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebserveApplicationTests {
 
+    @Autowired
+    private IAnnexe_taskService IAnnexe_taskService;
+
     @Test
     void contextLoads() {
+        IAnnexe_taskService.getById(1l);
 
     }
 

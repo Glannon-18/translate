f237d3031905dc058101b31fb4c020c43109f842
myName <rced87@aliyun.com>
2020-05-21 17:28:46 +0800

    生成文件翻译任务和词库中间表实体类接口

---
+++ webserve/src/main/java/com/vikey/webserve/service/IAtask_libService.java
@@ -0,0 +1,16 @@
+package com.vikey.webserve.service;
+
+import com.vikey.webserve.entity.Atask_lib;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author wkw
+ * @since 2020-05-21
+ */
+public interface IAtask_libService extends IService<Atask_lib> {
+
+}

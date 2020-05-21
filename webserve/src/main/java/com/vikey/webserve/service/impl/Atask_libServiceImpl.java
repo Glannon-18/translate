13a5bbc8efdd34e3bdb7ef0bc5618e8e17d304c0
myName <rced87@aliyun.com>
2020-05-21 17:27:56 +0800

    生成文件翻译任务和词库中间表实体类实现接口

---
+++ webserve/src/main/java/com/vikey/webserve/service/impl/Atask_libServiceImpl.java
@@ -0,0 +1,20 @@
+package com.vikey.webserve.service.impl;
+
+import com.vikey.webserve.entity.Atask_lib;
+import com.vikey.webserve.mapper.Atask_libMapper;
+import com.vikey.webserve.service.IAtask_libService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author wkw
+ * @since 2020-05-21
+ */
+@Service
+public class Atask_libServiceImpl extends ServiceImpl<Atask_libMapper, Atask_lib> implements IAtask_libService {
+
+}
